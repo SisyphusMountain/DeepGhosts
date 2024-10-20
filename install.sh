552f1342be83545e42401f0664e67b72a56a6b7c
enzo.marsot <enzo.marsot@ens-lyon.fr>
2024-10-20 21:27:43 +0200

    Modified install.sh to account for modified script paths

--- install.sh
+++ install.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-
+# Updated submodules path
 SUBMODULES=("extract_extant_script" "gene_transfer_script" "sample_script")
+SUBMODULES_DIR="rust_projects"
 
-DEST_DIR="resources"
-
+DEST_DIR="bin"
 
 mkdir -p $DEST_DIR
 mkdir -p Output
 
 for SUBMODULE in "${SUBMODULES[@]}"; do
   echo "Building $SUBMODULE..."
-  cd $SUBMODULE
+  cd $SUBMODULES_DIR/$SUBMODULE
 
   cargo build --release
 
-  mv target/release/$SUBMODULE ../$DEST_DIR/
-  cd ..
+  mv target/release/$SUBMODULE ../../$DEST_DIR/
+  cd ../..
   echo "$SUBMODULE built and moved successfully."
 done
 
